f667fe6d5f69300e782c9413697df6fd7ab35e05
张鹏举 <zhangpengju@thunisoft.com>
2018-09-29 17:53:37 +0800

    更新实体包

---
+++ GreatSSMC/ssmc-entry/src/main/java/com/great/entry/User.java
@@ -0,0 +1,12 @@
+package com.great.entry;
+
+/**
+ * @Author ZhPJ
+ * @Date 2018/9/29 002917:52
+ * @Version 1.0
+ * @Description:
+ */
+public class User {
+    private String id;
+    private String name;
+}
---
+++ GreatSSMC/ssmc-entry/src/main/java/com/great/entry/extended/UserExtended.java
@@ -0,0 +1,13 @@
+package com.great.entry.extended;
+
+import com.great.entry.User;
+
+/**
+ * @Author ZhPJ
+ * @Date 2018/9/29 002917:52
+ * @Version 1.0
+ * @Description:
+ */
+public class UserExtended extends User {
+
+}
